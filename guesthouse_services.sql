1bdda11dbace03e726123c74fb0bd557d6a4278a
Dinesh Chukkala <dineshchukkala169@gmail.com>
2022-11-14 21:36:06 +0530

    added tables for staff in guest house services

--- guesthouse_services.sql
+++ guesthouse_services.sql
@@ -13,3 +13,31 @@
 *************************************/
 
 -- Refer Page-1 of flowchar.drawio
+
+--  _Designation Table
+CREATE TABLE `_Designation` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    designation VARCHAR(50) NOT NULL,
+    salary INT
+);
+
+--  Staff Table
+CREATE TABLE `Staff` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    phone_no VARCHAR(15) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    address TEXT,
+    designation_id INT NOT NULL,
+    on_duty BOOLEAN,
+    FOREIGN KEY (designation_id) REFERENCES `_Designation`(id)
+);
+
+--  DutyLog Table
+CREATE TABLE `DutyLog` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL,
+    checkin_time DATETIME,
+    checkout_time DATETIME,
+    FOREIGN KEY (staff_id) REFERENCES `Staff`(id)
+);
