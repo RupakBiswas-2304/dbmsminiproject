9163c286ee90d1e6b1390c3564aac194dd4edfe0
Dinesh Chukkala <dineshchukkala169@gmail.com>
2022-11-14 20:08:56 +0530

    updated Tasks

---
+++ TASKS.md
@@ -0,0 +1,26 @@
+# TASKS
+
+## Triggers
+
+- ### GuestHouse
+  - Vacancy and Maintenance of Room upon Booking.
+  - onDuty should be updated on the bases of DutyLog.
+- ### MarketPlace
+
+## Procedures and Functions
+
+- ### GuestHouse
+  - Room Booking.
+  - Bill Generation.
+  - Check-in & Check-out for staff.
+  - Food Booking.
+- ### MarketPlace
+
+## Possible Columns to be Indexed
+
+- ### GuestHouse
+    - #### Food Item
+        - Availability.
+    - #### booking
+        - 
+- ### MarketPlace
---
+++ guesthouse_services.sql
@@ -0,0 +1,15 @@
+/************************************
+*************************************
+** Mini Project                    **
+**                                 **
+** Authors:                        **
+**   - Rupak Biswas                **
+**   - Aman Agarwal                **
+**   - Sanskriti Singh             **
+**   - Dinesh Chukkala             **
+**                                 **
+** File: guesthouse_services.sql   **
+*************************************
+*************************************/
+
+-- Refer Page-1 of flowchar.drawio
